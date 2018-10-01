dfc2c9f2fa471bbfe0ffe2d939de4aa8d278eacf
Joni <j.atisto@gmail.com>
2018-10-01 10:55:49 +0600

    made new branch release52.3

--- MyFirstMVC/MyFirstMVC/Models/Comment.cs
+++ MyFirstMVC/MyFirstMVC/Models/Comment.cs
@@ -6,6 +6,7 @@ namespace MyFirstMVC.Models
     public class Comment
     {
         public int Id { get; set; }
+        [Display(Name = "Имя")] public string Name { get; set; }
         [Display(Name = "Отзывы")] public string _comments { get; set; }
 
         [Display(Name = "Id Phone")] public int? PhoneId { get; set; }
